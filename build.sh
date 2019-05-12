b506b13429c38b06e732cfe984925a7c718e5882
PrabhaVeerubhotla <vvlprabha@gmail.com>
2019-05-11 18:48:06 -0700

    modifying build.sh to generate different g++ compiler versions
    Signed-off-by: PrabhaVeerubhotla <vvlprabha@gmail.com>

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 
 # non-threaded (gcc)
 echo "building non-threaded version"
-g++-8 -std=c++11 -L/usr/local/lib  matrix.cpp -o matrix-plain
+g++-8 -std=c++11 -L/usr/local/lib  src/matrix/matrix.cpp -o matrix-plain
 
 # gcc
 echo "building GNU version"
-g++-8 -std=c++11 -fopenmp -L/usr/local/lib  matrix.cpp -o matrix-gcc
+g++-8 -std=c++11 -fopenmp -L/usr/local/lib  src/matrix/matrix.cpp -o matrix-gcc
 
 # xcode (mac osx)
 echo "building xcode (LLVM) version"
-g++ -std=c++11 -Xclang -fopenmp -L/usr/local/lib  matrix.cpp -o matrix-xcode /usr/local/lib/libomp.dylib
+g++ -std=c++11 -Xclang -fopenmp -L/usr/local/lib  src/matrix/matrix.cpp -o matrix-xcode /usr/local/lib/libomp.dylib
